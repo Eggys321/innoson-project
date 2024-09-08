ddde9455948a8751a7a99c3198c2eae208698bef
eguono imonieroh <imonieroheguono@gmail.com>
2024-09-08 18:22:02 +0100

    fixes:worked on hero componenet

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+import Hero from './components/Hero'
 import Navbar from './layouts/Navbar'
 
 function App() {
   return (
     <>
     <Navbar/>
+    <Hero/>
      
     </>
   )
---
+++ src/components/Hero.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import "../styles/Hero.css";
+
+const Hero = () => {
+  return (
+    <>
+    <main>
+        <h2 className='container text-white'>Crafting</h2>
+    </main>
+    </>
+  )
+}
+
+export default Hero
